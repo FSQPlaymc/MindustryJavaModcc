3a7bf21e661a860e2effefccaed25293cb9c5e7d
FSQPlaymc <13077977318@163.com>
2025-06-20 10:13:49 +0800

    Update gradle-wrapper.properties
    Signed-off-by: FSQPlaymc <13077977318@163.com>

--- src/content/NewPT.java
+++ src/content/NewPT.java
@@ -1,13 +1,29 @@
 package content;
 
+import mindustry.content.Fx;
+import mindustry.entities.bullet.BasicBulletType;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
 import mindustry.world.Block;
 import mindustry.world.blocks.defense.turrets.ItemTurret;
 
 public class NewPT {
     public static Block NGM;
     public static void NewP(){
-        NGM=new ItemTurret("cspt"){{
+        NGM=new ItemTurret("capt"){{
+        requirements(Category.turret, ItemStack.with(Items.oreZeta,91));
+        ammo(
+              Items.oreZeta, new BasicBulletType(2.9f,9){{
+                    width = 7f;
+                    height = 9f;
+                    lifetime = 60f;
+                    ammoMultiplier = 2;
 
+                    hitEffect = despawnEffect = Fx.hitBulletColor;
+                  //  hitColor = backColor = trailColor = Pal.copperAmmoBack;
+                  //  frontColor = Pal.copperAmmoFront;
+                }}
+        );
         }};
     }
 }
