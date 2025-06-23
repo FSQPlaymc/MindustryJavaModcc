f15f35a7d53d7c0ffa7ce6539703b70d2959851c
FSQPlaymc <13077977318@163.com>
2025-06-23 13:44:46 +0800

    Update gradle-wrapper.properties
    Signed-off-by: FSQPlaymc <13077977318@163.com>

--- src/content/GG_Block.java
+++ src/content/GG_Block.java
@@ -1,9 +1,16 @@
 package content;
 
 
+import mindustry.content.Fx;
+import mindustry.content.Items;
+import mindustry.content.Liquids;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
 import mindustry.world.blocks.environment.OreBlock;
+import mindustry.world.blocks.production.GenericCrafter;
 
 public class GG_Block {
+    public static GenericCrafter chuangZaolu;
     public static OreBlock oreZeta;
     public static void Ore(){
         oreZeta=new OreBlock("zeta"){{
@@ -11,8 +18,27 @@ public class GG_Block {
             oreDefault = true;
             oreThreshold = 0.71f;
             oreScale = 10.47619f;
-            variants = 3;//三种贴图
+            variants = 3;//三种贴图在blocks/environment中
             //fullIcon=Core.atlas.find("custom-ores/ore_zeta");
+        }};
+        chuangZaolu = new GenericCrafter("chuangzaolu"){{//贴图在
+        requirements(Category.crafting, ItemStack.with(GGItems.zeta,30,
+                Items.copper,50,
+                Items.surgeAlloy,10,
+                Items.silicon,40,
+                Items.titanium,70,
+                Items.plastanium,30
+                ));
+            craftEffect = Fx.pulverizeMedium;
+            outputItem = new ItemStack(GGItems.itemCZzw, 2);//产出物品
+            craftTime = 300f;//生产时间
+            size = 4;
+            hasItems = true;//消耗物品
+            hasLiquids = true;//消耗流体
+            hasPower = true;//消耗电力
+            consumePower(90.0f);//*100
+            consumeItem(GGItems.zeta, 10);
+            consumeLiquid(Liquids.oil, 1.9f);
 
         }};
     }
