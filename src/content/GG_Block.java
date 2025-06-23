fa2ba43fbb0df84df8014caec23b00052aeaa395
FSQPlaymc <13077977318@163.com>
2025-06-23 08:59:36 +0800

    Update gradle-wrapper.properties
    Signed-off-by: FSQPlaymc <13077977318@163.com>

--- src/content/GG_Block.java
+++ src/content/GG_Block.java
@@ -6,11 +6,12 @@ import mindustry.world.blocks.environment.OreBlock;
 public class GG_Block {
     public static OreBlock oreZeta;
     public static void Ore(){
-        oreZeta=new OreBlock("ore_Zeta"){{
+        oreZeta=new OreBlock("ore_gzeta"){{
             itemDrop = GGItems.itemZeta; // 掉落物（必须）
             oreDefault = true;
             oreThreshold = 0.71f;
             oreScale = 10.47619f;
+            fullIcon=Core.atlas.find("custom-ores/ore_gzeta");
 
         }};
     }
