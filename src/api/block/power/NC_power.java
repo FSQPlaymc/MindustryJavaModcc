01b8528890bdc33dc9273c17eca6b2cd2285c80d
FSQPlaymc <13077977318@163.com>
2025-08-08 07:47:51 +0800

    master

--- src/api/block/power/NC_power.java
+++ src/api/block/power/NC_power.java
@@ -5,9 +5,10 @@ import content.GG_Block.GG_factory;
 import mindustry.Vars;
 import mindustry.gen.Building;
 import mindustry.world.blocks.power.ConsumeGenerator;
+import mindustry.world.blocks.power.NuclearReactor;
 import mindustry.world.blocks.power.PowerGenerator;
 
-public class NC_power extends ConsumeGenerator {
+public class NC_power extends NuclearReactor {
     public boolean structureComplete = false;
     public int progress = 0;
     public final int MAX_PROGRESS = 100;
@@ -17,10 +18,13 @@ public class NC_power extends ConsumeGenerator {
         solid = true;
         configurable = true;
     }
-    static int factoryCount = 0; // 计数器：符合要求的方块数量
-    static int factoryS =0;
-    static int FL;
-    static int FS;
+    private int factoryX = 0;
+    private int factoryY = 0;
+    private int FL = 0;
+    private int FS = 0;
+    private int[][] asdf;
+    private int checkX;
+    private int checkY;
     public class NC_powerBuid extends PowerGenerator.GeneratorBuild{
             public void update() {
                 // 获取建筑所在的主 Tile 坐标
@@ -40,13 +44,13 @@ public class NC_power extends ConsumeGenerator {
                 if (neighborL != null && neighborL.block == GG_factory.cs) {
                     // 邻居是 GG_factory.cs 方块
                     // 循环检测左侧N个方块
-                    for (int i = 1; i <= 64; i++) {
-                        int checkX = tileX - i; // 左侧第i个位置
+                    for (int i = 2; i <= 64; i=i+2) {
+                         checkX = tileX - i; // 左侧第i个位置
                         Building neighbor = Vars.world.build(checkX, tileY);
 
                         // 检查方块是否存在且为GG_factory.cs
                         if (neighbor != null && neighbor.block == GG_factory.cs) {
-                            factoryCount--;
+                            factoryX=factoryX-2;
                             // 在这里添加对每个检测到的工厂的操作
                             // 例如：GG_factory.GG_factoryBuild factory = (GG_factory.GG_factoryBuild) neighbor;
                             // factory.doSomething();
@@ -57,13 +61,13 @@ public class NC_power extends ConsumeGenerator {
                     }
                 }else {
                     // 循环检测右侧N个方块
-                    for (int i = 1; i <= 64; i++) {
-                        int checkX = tileX + i; // 左侧第i个位置
+                    for (int i = 2; i <= 64; i=i+2) {
+                         checkX = tileX + i; // 左侧第i个位置
                         Building neighbor = Vars.world.build(checkX, tileY);
 
                         // 检查方块是否存在且为GG_factory.cs
                         if (neighbor != null && neighbor.block == GG_factory.cs) {
-                            factoryCount++;
+                            factoryX=factoryX+2;
                             // 在这里添加对每个检测到的工厂的操作
                             // 例如：GG_factory.GG_factoryBuild factory = (GG_factory.GG_factoryBuild) neighbor;
                             // factory.doSomething();
@@ -75,13 +79,13 @@ public class NC_power extends ConsumeGenerator {
                 }if (neighborS != null && neighborS.block == GG_factory.cs) {
                     // 邻居是 GG_factory.cs 方块
                     // 循环检测下侧N个方块
-                    for (int i = 1; i <= 64; i++) {
-                        int checkY = tileY - i; // 下侧第i个位置
+                    for (int i = 2; i <= 64; i=i+2) {
+                         checkY = tileY - i; // 下侧第i个位置
                         Building neighbor = Vars.world.build(tileX, checkY);
 
                         // 检查方块是否存在且为GG_factory.cs
                         if (neighbor != null && neighbor.block == GG_factory.cs) {
-                            factoryS--;
+                            factoryY = factoryY -2;
                             // 在这里添加对每个检测到的工厂的操作
                             // 例如：GG_factory.GG_factoryBuild factory = (GG_factory.GG_factoryBuild) neighbor;
                             // factory.doSomething();
@@ -92,12 +96,12 @@ public class NC_power extends ConsumeGenerator {
                     }
                 }else {
                     // 循环检测上侧N个方块
-                    for (int i = 1; i <= 64; i++) {
-                        int checkY = tileY + i; // 上侧第i个位置
+                    for (int i = 2; i <= 64; i=i+2) {
+                         checkY = tileY + i; // 上侧第i个位置
                         Building neighbor = Vars.world.build(tileX, checkY);
                         // 检查方块是否存在且为GG_factory.cs
                         if (neighbor != null && neighbor.block == GG_factory.cs) {
-                            factoryS++;
+                            factoryY = factoryY +2;
                             // 在这里添加对每个检测到的工厂的操作
                             // 例如：GG_factory.GG_factoryBuild factory = (GG_factory.GG_factoryBuild) neighbor;
                             // factory.doSomething();
@@ -107,8 +111,37 @@ public class NC_power extends ConsumeGenerator {
                         }
                     }
                 }
-                if (factoryCount<0){FL= factoryCount*-1;}
-                if (factoryS<0){FS= factoryS*-1;}
+                if (factoryX<0){FL= factoryX*-1;}
+                if (factoryY <0){FS= factoryY *-1;}
+                asdf =new int[FL][FS];
+                int L=FL;
+                int S=FS;
+                int X=checkX;
+                int Y=checkY;
+                for (;;){
+                    Building neighboru = Vars.world.build(X, Y);
+                    if (neighboru != null && neighboru.block == GG_factory.cs){
+                        asdf[L][S]=90;
+                    }else if (neighboru != null && neighboru.block == GG_factory.SL){
+                        asdf[L][S]=1;
+                    }else if (neighboru != null && neighboru.block == GG_factory.fanying){
+                        asdf[L][S]=80;
+                    }else {asdf[L][S]=-1;}
+                    L=L-2;
+                    if (L<0){L=FL;
+                        S=S-2;if (S<0){
+                            break;
+                        }
+                    }
+                    X=X-2;
+                    if (X<tileX){
+                        X=checkX;
+                        Y=Y-2;if (Y<0){
+                            break;
+                        }
+                    }
+                }
+
 
         }
         @Nullable
